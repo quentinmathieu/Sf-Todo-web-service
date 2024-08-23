131cd0307d8e869f39a06eef10ef32acf41f0ee6
quentinmathieu <quentin.mth@gmail.com>
2024-08-23 09:51:13 +0200

    #[ApiResource] added

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ApiResource]
 class Task
 {
     #[ORM\Id]
--- src/Entity/TaskList.php
+++ src/Entity/TaskList.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TaskListRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskListRepository::class)]
+#[ApiResource]
 class TaskList
 {
     #[ORM\Id]
